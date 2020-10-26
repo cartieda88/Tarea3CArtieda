6fcc19585e780e17d82e3dc5ff4ba7b29d08ded7
Cristhian Artieda <cartieda@netlife.net.ec>
2020-10-26 15:33:49 -0500

    V2

--- Tarea2CArtieda/MainPage.xaml.cs
+++ Tarea2CArtieda/MainPage.xaml.cs
@@ -14,67 +14,25 @@ namespace Tarea2CArtieda
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        int suma1;
-        int suma2;
-        int suma3;
-
+        
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void btnSumar_clicked(object sender, EventArgs e)
+
+        private async void btnAbrirVistaDos_clicked(object sender, EventArgs e)
         {
-            try
-            {
-                double dato1 = Convert.ToDouble(txtDato1.Text);
-                double dato2 = Convert.ToDouble(txtDato2.Text);
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
 
-                double s1 = dato1 * (0.30);
-                double e1 = dato2 * (0.20);
-                double suma = s1 + e1;
-                txtResultado.Text = suma.ToString();
+            if (user == "estudiante2020" && pass == "uisrael2020")
+            {
+                await Navigation.PushAsync(new Vista_Dos(user, pass));
             }
-            catch (Exception ex)
-            {
-                DisplayAlert("Mensaje de Alerta", "ERROR" + ex.Message, "Aceptar");
-            }
-        }
+            else
+                await DisplayAlert("Sistemas de Notas", "Usuario o Clave son incorrectos!!", "Cerrar");
 
-        private void btnSumar2_clicked(object sender, EventArgs e)
-        {
-            try
-            {
-                double dato3 = Convert.ToDouble(txtDato3.Text);
-                double dato4 = Convert.ToDouble(txtDato4.Text);
-
-                double s2 = dato3 * (0.30);
-                double e2 = dato4 * (0.20);
-                double suma2 = s2 + e2;
-                txtResultado2.Text = suma2.ToString();
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("Mensaje de Alerta", "ERROR" + ex.Message, "Aceptar");
-            }
-        }
-
-        private void btnSumar3_clicked(object sender, EventArgs e)
-        {
-           
-                double suma1 = Convert.ToDouble(txtResultado.Text);
-                double suma2 = Convert.ToDouble(txtResultado2.Text);
-
-                double f1 = suma1;
-                double f2 = suma2;
-                double suma3 = f1 + f2;
-                txtResultado3.Text = suma3.ToString();
-
-            if (suma3 > 7)
-            {
-                DisplayAlert("Estado del Estudiante","Aprobado..!", "Guardar");
-            } 
-            else  DisplayAlert("Estado del Estudiante", "Reprobado..!", "Guardar");
         }
 
     }
